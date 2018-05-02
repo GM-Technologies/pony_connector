5fd10c6b4977508917c607c2854bd40e4fe84c70
Kukhapprabu Ramasamy <kukhapprabu@gmail.com>
2018-05-02 10:42:04 +0530

    Homepage added / Static URL added

--- connector/views.py
+++ connector/views.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    text = '<div style="position: fixed; display: flex; align-items: center; top: 0;' \
+           ' font-family: \'Helvetica Neue\',Helvetica,Arial,sans-serif; left: 0; width: ' \
+           '100%; height: 100%; background-color: white; text-align: center; z-index: ' \
+           '1000000;"><h4 style="width:100%">Gladminds Administration<br>Permission ' \
+           'denied</h4></div>'
+    return HttpResponse(text)
