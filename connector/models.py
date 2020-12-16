769b037194e80c5862a5c27c274688a520f419e4
kapil <kapil@gladminds.co>
2020-12-16 12:11:29 +0530

    added closeflag for invoice header

--- connector/models.py
+++ connector/models.py
@@ -526,6 +526,7 @@ class InvoiceHeader(base_models.AuditModel):
     total_sgst_amount = models.FloatField(db_column='TOT_SGSTAMT')
     total_igst_amount = models.FloatField(db_column='TOT_IGSTAMT')
     invoice_gstin = models.CharField(max_length=15, db_column='INV_GSTIN', null=True, blank=True)
+    close_flag = models.IntegerField(null=True, blank=True, db_column='CFLAG')
 
     def __str__(self):
         return "{}".format(self.invoice_number)
@@ -568,6 +569,7 @@ class InvoiceHeader(base_models.AuditModel):
             "total_sgst_amount": str(self.total_sgst_amount or 0),
             "total_igst_amount": str(self.total_igst_amount or 0),
             "invoice_gstin": self.invoice_gstin or "",
+            'close_flag': str(self.close_flag or ""),
             "invoice_details": [each.to_json() for each in self.invoicedetails_set.all()],
             "is_sync": self.is_sync
         }
